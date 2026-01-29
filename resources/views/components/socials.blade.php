a4eb89d66182bbf24f1ac3fc3dd7ce915cd19faf
josh20122 <jamesjoshua20122@gmail.com>
2026-01-29 10:47:15 +0300

    hnaje

--- resources/views/components/socials.blade.php
+++ resources/views/components/socials.blade.php
@@ -1,7 +1,7 @@
 <div class="fixed z-50 bottom-10 left-4">
-    <a href="https://chat.whatsapp.com/GjLo2i7SUO1IZdTWhNWrlw" target="__blank">
+    <a href="https://chat.whatsapp.com/DVXB7kQIEPi8FoGrCfqaqg" target="__blank">
 
-        <img src="/assets/images/whatsapp-icon.png" class="h-10 rounded-t-full rounded-r-full shadow-md shadow-yellow-500"
-            alt="">
+        <img src="/assets/images/whatsapp-icon.png"
+            class="h-10 rounded-t-full rounded-r-full shadow-md shadow-yellow-500" alt="">
     </a>
 </div>
