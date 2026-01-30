20811d767939972181873ab3daa22e3094dad298
josh20122 <jamesjoshua20122@gmail.com>
2026-01-30 12:28:22 +0300

    chnwbndjkwd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -122,7 +122,7 @@
                     </div>
                 </div>
 
-                <div class="row mt-50">
+                {{-- <div class="row mt-50">
                     <div class="col-lg-12">
                         <div class="table-responsive--md p-0">
                             <div class=" ">
@@ -130,50 +130,50 @@
                                 Lustre rewards
                             </div>
                             @if (count($data['tasks']) > 0)
-                                <div class="flex flex-col gap-y-4">
+                            <div class="flex flex-col gap-y-4">
 
-                                    @foreach ($data['tasks'] as $task)
-                                        <form action="/ranking/claim" method="POST">
-                                            @csrf
-                                            <div class="  rounded-md  bg-black flex px-2 py-3 flex-col gap-y-2 text-white ">
-                                                <div class="
+                                @foreach ($data['tasks'] as $task)
+                                <form action="/ranking/claim" method="POST">
+                                    @csrf
+                                    <div class="  rounded-md  bg-black flex px-2 py-3 flex-col gap-y-2 text-white ">
+                                        <div class="
                                             inline-flex w-full justify-between ">
-                                                    <div> {{ $task['task_text'] }} </div>
-                                                    <div> {{ $data['activeReferrals'] }}/{{ (int) $task['task'] }}
-                                                    </div>
-                                                </div>
-
-
-                                                <input type="hidden" value="{{ $task['id'] }}" name="task">
-                                                <div class=" w-full  flex gap-x-3">
-                                                    <progress class=" w-full" value="{{ $data['activeReferrals'] }}"
-                                                        max="{{ $task['task'] }}"></progress>
-                                                    <div class=" text-sm">
-                                                        {{ ($data['activeReferrals'] * 100) / (int) $task['task'] }}%
-                                                    </div>
-                                                </div>
-
-                                                <div class=" w-full ">
-                                                    <button type="submit"
-                                                        class="btn   disabled:border-primary rounded-sm btn-primary hover:bg-yellow-500 bg-yellow-500 border-0 btn-sm text-white">
-                                                        {{ $task['completed'] ? 'COMPLETED' : 'REDEEM' }}
-                                                    </button>
-                                                </div>
+                                            <div> {{ $task['task_text'] }} </div>
+                                            <div> {{ $data['activeReferrals'] }}/{{ (int) $task['task'] }}
                                             </div>
-                                        </form>
-                                    @endforeach
-                                </div>
-                            @else
-                                <div class=" rounded-md bg-black text-white p-2">
-                                    <div class="flex flex-col gap-y-3">
-                                        <div class="text-red-600 text-center"> No tasks found </div>
+                                        </div>
 
+
+                                        <input type="hidden" value="{{ $task['id'] }}" name="task">
+                                        <div class=" w-full  flex gap-x-3">
+                                            <progress class=" w-full" value="{{ $data['activeReferrals'] }}"
+                                                max="{{ $task['task'] }}"></progress>
+                                            <div class=" text-sm">
+                                                {{ ($data['activeReferrals'] * 100) / (int) $task['task'] }}%
+                                            </div>
+                                        </div>
+
+                                        <div class=" w-full ">
+                                            <button type="submit"
+                                                class="btn   disabled:border-primary rounded-sm btn-primary hover:bg-yellow-500 bg-yellow-500 border-0 btn-sm text-white">
+                                                {{ $task['completed'] ? 'COMPLETED' : 'REDEEM' }}
+                                            </button>
+                                        </div>
                                     </div>
+                                </form>
+                                @endforeach
+                            </div>
+                            @else
+                            <div class=" rounded-md bg-black text-white p-2">
+                                <div class="flex flex-col gap-y-3">
+                                    <div class="text-red-600 text-center"> No tasks found </div>
+
                                 </div>
+                            </div>
                             @endif
                         </div>
                     </div>
-                </div>
+                </div> --}}
 
 
 
